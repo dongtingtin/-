839f71fed6452c81a03d47d1499cd8018b0a1558
dongtingtin <1157098533@qq.com>
2016-11-11 11:05:52 +0800

    j

---
+++ one.ts
@@ -0,0 +1,3 @@
+我的第一个项目1111111
+
+git config --global NavigatorUserMedia.name dongtingtin
---
+++ test1.js
@@ -0,0 +1,9 @@
+var num = 1;
+var myname = '杨杰';
+//console.log(myname.indexOf('y'）)
+if (myname.indexOf('杨') != -1) {
+    console.log('杨字在字符串离第' + myname.indexOf('杨') + '个字');
+}
+else {
+    console.log('字符串');
+}
